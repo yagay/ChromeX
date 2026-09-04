6dca03d29d85053b47bb5387331dd50a47fb3779
Jie <1614094+yagay@users.noreply.github.com>
2026-09-04 22:44:05 +0100

    refactor: add ChromeX libxposed API 102 entry point

---
+++ app/src/main/java/com/yagay/chromex/ModuleMain.java
@@ -0,0 +1,50 @@
+package com.yagay.chromex;
+
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import io.github.libxposed.api.XposedModule;
+
+public final class ModuleMain extends XposedModule {
+    private SharedPreferences prefs;
+    private HookSupport hooks;
+
+    @Override
+    public void onModuleLoaded(ModuleLoadedParam param) {
+        log(Log.INFO, "ChromeX", "module loaded in " + param.getProcessName()
+                + ", framework=" + getFrameworkName() + " " + getFrameworkVersion()
+                + ", api=" + getApiVersion());
+    }
+
+    @Override
+    public void onPackageReady(PackageReadyParam param) {
+        if (!Chrome145.PACKAGE.equals(param.getPackageName())) return;
+        if (!param.isFirstPackage()) return;
+
+        ClassLoader loader = param.getClassLoader();
+        prefs = Config.fromModule(this);
+        hooks = new HookSupport(this);
+
+        new TabHooks(this, hooks, prefs, loader).install();
+        new DownloadDialogHooks(this, hooks, prefs, loader).install();
+        new InstallerHooks(this, hooks, prefs, loader).install();
+        new BannerHooks(this, hooks, prefs, loader).install();
+
+        log(Log.INFO, "ChromeX", "Chrome 145 feature hooks installed");
+    }
+
+    @Override
+    public boolean onHotReloading(HotReloadingParam param) {
+        return true;
+    }
+
+    @Override
+    public void onHotReloaded(HotReloadedParam param) {
+        for (HookHandle handle : param.getOldHookHandles()) {
+            try {
+                handle.unhook();
+            } catch (Throwable ignored) {}
+        }
+        log(Log.INFO, "ChromeX", "old hooks removed after hot reload; restart Chrome to reinstall");
+    }
+}
