bd9196ef60b814626c3f81c81ef8277794698d4f
Jie <1614094+yagay@users.noreply.github.com>
2026-09-05 12:11:15 +0100

    Test reverse Chromium download-name mapping

--- app/src/test/java/com/yagay/chromex/DownloadNamePolicyTest.java
+++ app/src/test/java/com/yagay/chromex/DownloadNamePolicyTest.java
@@ -1,6 +1,8 @@
 package com.yagay.chromex;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -20,5 +22,21 @@ public class DownloadNamePolicyTest {
         assertFalse(DownloadNamePolicy.matchesUniquifiedName("app.apk", "app (x).apk"));
         assertFalse(DownloadNamePolicy.matchesUniquifiedName("app.apk", "other (1).apk"));
         assertFalse(DownloadNamePolicy.matchesUniquifiedName("app.apk", "app (1).zip"));
+    }
+
+    @Test
+    public void reversesChromiumUniquifiedNames() {
+        assertEquals("app.apk", DownloadNamePolicy.originalNameFromUniquified("app (1).apk"));
+        assertEquals("archive.tar.gz",
+                DownloadNamePolicy.originalNameFromUniquified("archive.tar (12).gz"));
+        assertEquals("README", DownloadNamePolicy.originalNameFromUniquified("README (7)"));
+    }
+
+    @Test
+    public void doesNotReverseNonChromiumNames() {
+        assertNull(DownloadNamePolicy.originalNameFromUniquified("app.apk"));
+        assertNull(DownloadNamePolicy.originalNameFromUniquified("app(x).apk"));
+        assertNull(DownloadNamePolicy.originalNameFromUniquified("app (x).apk"));
+        assertNull(DownloadNamePolicy.originalNameFromUniquified("(1).apk"));
     }
 }
